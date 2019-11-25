96698428cc7e4ba3481e04dfd7022a1ab44fae45
XuSiXiao <1907565892@qq.com>
2019-11-25 15:00:16 +0800

    添加数据库

---
+++ library.sql
@@ -0,0 +1,92 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50630
+Source Host           : localhost:3306
+Source Database       : library
+
+Target Server Type    : MYSQL
+Target Server Version : 50630
+File Encoding         : 65001
+
+Date: 2019-11-25 14:53:17
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for books
+-- ----------------------------
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `ID` int(11) NOT NULL,
+  `bookName` varchar(255) DEFAULT NULL,
+  `bookAuthor` varchar(255) DEFAULT NULL,
+  `bookPicture` varchar(255) DEFAULT NULL,
+  `bookPrice` double DEFAULT NULL,
+  `creationTime` time DEFAULT NULL,
+  `updateTime` time DEFAULT NULL,
+  `bookTotal` int(255) DEFAULT NULL,
+  `bookQuantity` int(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of books
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for borrow
+-- ----------------------------
+DROP TABLE IF EXISTS `borrow`;
+CREATE TABLE `borrow` (
+  `ID` int(11) NOT NULL,
+  `userName` varchar(255) DEFAULT NULL,
+  `bookName` varchar(255) DEFAULT NULL,
+  `quantity` int(255) DEFAULT NULL,
+  `startTime` time DEFAULT NULL,
+  `endTime` time DEFAULT NULL,
+  `returnTime` time DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of borrow
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for librarian
+-- ----------------------------
+DROP TABLE IF EXISTS `librarian`;
+CREATE TABLE `librarian` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `creationTime` time DEFAULT NULL,
+  `updateTime` time DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of librarian
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `userName` varchar(255) DEFAULT NULL,
+  `userPassword` varchar(255) DEFAULT NULL,
+  `phone` int(255) DEFAULT NULL,
+  `creationTime` time DEFAULT NULL,
+  `updateTime` time DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
